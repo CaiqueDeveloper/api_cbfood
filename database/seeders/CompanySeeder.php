3c59e13f396c6bd5c3a4d5f196e2a1fa56ebb997
Caique Bispo <caiquebispo0023@gmail.com>
2022-03-31 08:40:08 -0300

    Craiando a Seeder Company

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('companies')->insert([
+            'name' => 'Company Teste  ',
+            'email' => 'caompanyteste@gmail.com',
+            'cnpj' => '35089909000142',
+       ]);
+    }
+}
